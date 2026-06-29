ee97ea6dfa661082d00046097331fdbd6ff446d6
Eli Carhuatocto <51203238+ElizabethCO25@users.noreply.github.com>
2026-06-29 15:59:59 -0500

    Cambio de datos de prueba en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Modalab-Classroom/',
+  // Debe ser el nombre exacto del repositorio entre barras
+  base: '/Modalab-Classroom/', 
   build: {
     outDir: 'dist',
-    sourcemap: true
   }
 })
